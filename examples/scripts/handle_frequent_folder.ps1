02c3b4843ec65e1b9229dcd833276ba0e4fd2c2d
Hellager <steinsgu@gmail.com>
2024-12-03 20:28:17 +0800

    feat: add handle frequent folder script

---
+++ examples/scripts/handle_frequent_folder.ps1
@@ -0,0 +1,15 @@
+# !!! This script may be fully stucked on some system, only close window will stop the process
+
+$OutputEncoding = [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$shell = New-Object -ComObject Shell.Application
+
+$folder = $PSScriptRoot
+
+$userInput = Read-Host "Try pin folder '$folder' to frequent folder, confirm to continue('y' to proceed)"
+if ($userInput -eq 'y' -or $userInput -eq 'yes') {
+    # `pintohome` is a toggle function, which means if you run this twice, the folder will be unpinned
+    $shell.NameSpace("$folder").Self.InvokeVerb("pintohome")
+} else {
+    Write-Output "Stop proceeding, exiting script."
+    exit
+}
