80e8bb794c2ab5219749898711c5156cb7f0a898
Hellager <steinsgu@gmail.com>
2024-12-03 20:13:18 +0800

    feat: update refresh script to show window name

--- examples/scripts/refresh_explorer_windows.ps1
+++ examples/scripts/refresh_explorer_windows.ps1
@@ -1,9 +1,7 @@
 $OutputEncoding = [Console]::OutputEncoding = [System.Text.Encoding]::UTF8;
 $shellApplication = New-Object -ComObject Shell.Application;
 $windows = $shellApplication.Windows();
-$count = $windows.Count();
-
-foreach( $i in 0..($count-1) ) {
-	$item = $windows.Item( $i )
-	$item.Refresh() 
+$windows | ForEach-Object { 
+    Write-Host "Refreshing window: $($_.LocationName)"
+    $_.Refresh() 
 }
