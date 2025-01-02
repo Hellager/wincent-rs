9e427d6445a1d1433ac6bbe2c2e4590c23e65032
Hellager <steinsgu@gmail.com>
2025-01-02 20:48:58 +0800

    feat: update folder script

--- examples/scripts/handle_frequent_folder.ps1
+++ examples/scripts/handle_frequent_folder.ps1
@@ -8,7 +8,7 @@ $folder = $PSScriptRoot
 $userInput = Read-Host "Try pin folder '$folder' to frequent folder, confirm to continue('y' to proceed)"
 if ($userInput -eq 'y' -or $userInput -eq 'yes') {
     # `pintohome` is a toggle function, which means if you run this twice, the folder will be unpinned
-    $shell.NameSpace("$folder").Self.InvokeVerb("pintohome")
+    $shell.NameSpace("$folder").Self.InvokeVerb('pintohome')
 } else {
     Write-Output "Stop proceeding, exiting script."
     exit
